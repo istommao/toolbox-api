39dc96020769440408197690d44e3fba4c549334
codingcat <istommao@gmail.com>
2020-09-30 17:55:42 +0800

    Add test folder

---
+++ runtest.sh
@@ -0,0 +1,1 @@
+pytest --cov=src tests --html=testreport/report.html
--- tests/functional/test_dateutils.py
+++ tests/functional/test_dateutils.py
@@ -1,3 +1,4 @@
+import datetime
 
 from src.apis import dateutils
 
@@ -8,3 +9,13 @@ def test_ts_to_datetime():
     dt_obj = dateutils.ts_to_datetime(ts_str)
 
     assert dt_obj.strftime('%Y-%m-%d %H:%M:%S') == '2020-09-30 15:59:27'
+
+
+def test_datetime_to_ts():
+
+    dt_obj = datetime.datetime(2020, 9, 30, 15, 59, 27)
+
+
+    ts_str = dateutils.datetime_to_ts(dt_obj)
+
+    assert ts_str == 1601452767
