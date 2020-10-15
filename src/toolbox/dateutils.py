ed4eac6723479aaeadad1ec332e633effe0daaa1
codingcat <istommao@gmail.com>
2020-10-15 14:36:44 +0800

    Add dateutils week month and day range function

--- src/toolbox/dateutils.py
+++ src/toolbox/dateutils.py
@@ -31,24 +31,34 @@ def datetime_to_ts(datetime_obj: object, ms=False):
     return timestamp
 
 
+def get_day_range(days: int = 0, unbounded=False):
+    """获取天的开始和结束时间.
+
+    :param unbounded: 开区间
+    """
+    today = datetime.date.today()
+    start_at = datetime.datetime.combine(today, datetime.time.min)
+
+    if unbounded:
+        end_at = datetime.datetime.combine(today + datetime.timedelta(days=1), datetime.time.min)
+    else:
+        end_at = datetime.datetime.combine(today, datetime.time.max)
+
+    start_at = start_at + datetime.timedelta(days=days)
+    end_at = end_at + datetime.timedelta(days=days)
+    return start_at, end_at
+
+
 def get_today_range(unbounded=False):
     """获取今天的开始和结束时间.
 
     :param unbounded: 开区间
     """
-    today = datetime.date.today()
-    start = datetime.datetime.combine(today, datetime.time.min)
-
-    if unbounded:
-        end = datetime.datetime.combine(today + datetime.timedelta(days=1), datetime.time.min)
-    else:
-        end = datetime.datetime.combine(today, datetime.time.max)
-
-    return start, end
+    return get_day_range(days=0, unbounded=unbounded)
 
 
-def get_this_week_range(start='monday', unbounded=False):
-    """获取今天的开始和结束时间.
+def get_week_range(weeks=0, start='monday', unbounded=False):
+    """获取周的开始和结束时间.
 
     :param start: monday or sunday
     :param unbounded: 开区间
@@ -83,4 +93,84 @@ def get_this_week_range(start='monday', unbounded=False):
         today + datetime.timedelta(days=end_diff_day), end_time
     )
 
+    start_at = start_at + datetime.timedelta(weeks=weeks)
+    end_at = end_at + datetime.timedelta(weeks=weeks)
+
     return start_at, end_at
+
+
+def get_this_week_range(start='monday', unbounded=False):
+    """获取这周的开始和结束时间.
+
+    :param start: monday or sunday
+    :param unbounded: 开区间
+    """
+    return get_week_range(weeks=0, start=start, unbounded=unbounded)
+
+
+def get_last_week_range(start='monday', unbounded=False):
+    """获取上周的开始和结束时间.
+
+    :param start: monday or sunday
+    :param unbounded: 开区间
+    """
+    return get_week_range(weeks=-1, start=start, unbounded=unbounded)
+
+
+def get_next_week_range(start='monday', unbounded=False):
+    """获取下周的开始和结束时间.
+
+    :param start: monday or sunday
+    :param unbounded: 开区间
+    """
+    return get_week_range(weeks=1, start=start, unbounded=unbounded)
+
+
+def get_month_range(months=0, unbounded=False):
+    """获取月的开始和结束时间.
+
+    :param unbounded: 开区间
+    """
+
+    today = datetime.date.today()
+    year_diff = (today.month + months) // 12
+
+    end = today.replace(
+        month=(today.month + months) % 12 + 1,
+        year=today.year + year_diff,
+        day=1
+    ) - datetime.timedelta(days=1)
+
+    start_at = end.replace(day=1)
+    start_at = datetime.datetime.combine(start_at, datetime.time.min)
+
+    if unbounded:
+        end_at = datetime.datetime.combine(end + datetime.timedelta(days=1), datetime.time.min)
+    else:
+        end_at = datetime.datetime.combine(end, datetime.time.max)
+
+    return start_at, end_at
+
+
+def get_this_month_range(unbounded=False):
+    """获取 当前月的开始和结束时间.
+
+    :param unbounded: 开区间
+    """
+    return get_month_range(months=0, unbounded=unbounded)
+
+
+def get_last_month_range(unbounded=False):
+    """获取 上个月的开始和结束时间.
+
+    :param unbounded: 开区间
+    """
+    return get_month_range(months=-1, unbounded=unbounded)
+
+
+def get_next_month_range(unbounded=False):
+    """获取 下个月的开始和结束时间.
+
+    :param unbounded: 开区间
+    """
+    return get_month_range(months=1, unbounded=unbounded)
